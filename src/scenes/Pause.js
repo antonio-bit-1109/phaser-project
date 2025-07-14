5951aa2d5e1a906fb3bcc55b07544d7bfc6f708e
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-07-14 17:55:50 +0200

    ADD: added class pause to handle pausing game

---
+++ src/scenes/Pause.js
@@ -0,0 +1,24 @@
+export class Pause extends Phaser.Scene {
+
+    // il constructor serve per dare un nome a questa classe, se la devo richiamare da qualche parte questo sarà il nome
+    constructor() {
+        super('pause');
+    }
+
+
+    init(data) {
+
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update(time, delta) {
+        super.update(time, delta);
+    }
+}
