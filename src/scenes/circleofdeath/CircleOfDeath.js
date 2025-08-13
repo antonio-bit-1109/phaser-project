6bb38798c4bd7e8f79fac096ed31c6aa933c9c6d
antonio <antonio.rizzuti@hotmail.com>
2025-08-13 12:53:51 +0200

    ADD: create CircleOfDeath scene class

---
+++ src/scenes/circleofdeath/CircleOfDeath.js
@@ -0,0 +1,23 @@
+export class CircleOfDeath  extends Phaser.Scene {
+
+    canvasWidth = null;
+    canvasHeight = null;
+
+    init(data) {
+        this.canvasWidth = data.canvasWidth;
+        this.canvasHeight = data.canvasHeight;
+    }
+
+    preload() {
+
+    }
+
+
+    create() {
+
+    }
+
+    update(time, delta) {
+
+    }
+}
