6f4c67b542089f9652ff7a73117b85cee5e2db33
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-08-18 22:11:24 +0200

    UPDATE: enhance DudeShipManager with HP sprite handling, animations, and heart shake effect

--- src/scenes/circleofdeath/entity/DudeShipManager.js
+++ src/scenes/circleofdeath/entity/DudeShipManager.js
@@ -9,6 +9,8 @@ export class DudeShipManager {
     HasTurboNeedRecharge = false
     turbo = false
     invincible = false
+    hp_sprite = null
+    hpPiece = 4
 
     constructor(scene) {
         this.dudeShip = null
@@ -18,6 +20,14 @@ export class DudeShipManager {
     }
 
     //getter
+
+    getHpSprite() {
+        return this.hp_sprite
+    }
+
+    getHpPiece() {
+        return this.hpPiece
+    }
 
     getTurbo() {
         return this.turbo
@@ -46,6 +56,10 @@ export class DudeShipManager {
 
     //setter
 
+    setHpPiece(num) {
+        this.hpPiece = num
+    }
+
     setTurbo(bool) {
         this.turbo = bool
     }
@@ -59,6 +73,68 @@ export class DudeShipManager {
         this.HasTurboNeedRecharge = bool;
     }
 
+
+    setHpBasedOnHpPiece() {
+        this.hpPiece--
+        this.shakeHeart()
+        switch (this.hpPiece) {
+            case 4:
+                console.log("vita piena. nessuna manovra.")
+                break;
+            case 3:
+                this.hp_sprite.play("h3/4")
+
+                break;
+            case 2:
+                this.hp_sprite.play("h2/4")
+                break;
+            case 1:
+                this.hp_sprite.play("h1/4")
+                break;
+            case 0:
+                this.hp_sprite.play("h0")
+                break;
+
+        }
+    }
+
+    shakeHeart() {
+
+        this.scene.tweens.chain({
+            targets: this.hp_sprite,
+            loop: 0,
+            tweens: [
+                {
+                    duration: 50,
+                    x: this.hp_sprite.x += 5
+                },
+                {
+                    duration: 50,
+                    x: this.hp_sprite.x -= 5
+                }, {
+                    duration: 50,
+                    x: this.hp_sprite.x += 5
+                }, {
+                    duration: 50,
+                    x: this.hp_sprite.x -= 5
+                }, {
+                    duration: 50,
+                    x: this.hp_sprite.x += 5
+                }, {
+                    duration: 50,
+                    x: this.hp_sprite.x -= 5
+                }, {
+                    duration: 50,
+                    x: this.hp_sprite.x += 5
+                },
+                {
+                    duration: 50,
+                    x: this.hp_sprite.x -= 5
+                },
+            ]
+        })
+    }
+
     create(w, h) {
         this.canvasH = h
         this.canvasW = w
@@ -68,12 +144,20 @@ export class DudeShipManager {
 
         this.boostCloud_group = this.scene.add.group()
         this.createTurboBars()
+        this.createHpSprite()
     }
 
     update(delta) {
 
         this.rechargeTurbo(delta)
 
+    }
+
+    createHpSprite() {
+        this.hp_sprite = this.scene.add.sprite(this.canvasW / 12, this.canvasH / 6, "heartsSpriteSheet")
+            .setDepth(5)
+            .setScale(3)
+            .play("hFull")
     }
 
     createTurboBars() {
