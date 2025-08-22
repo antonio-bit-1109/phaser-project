d38a1925d25d8fd3489e501f2509db3be426be6b
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-08-22 17:31:37 +0200

    ADD: introduce helper functions for angle calculations, update constants in CircleOfDeath scene

--- src/scenes/circleofdeath/constants/costanti.js
+++ src/scenes/circleofdeath/constants/costanti.js
@@ -1,5 +1,46 @@
 export const costanti = {
     raggio: 270,
     REDTRACE: "red",
-    BLACKTRACE: "black"
+    BLACKTRACE: "black",
+    minNumRifleAim: 15,
+    maxNumRifleAim: 22,
+    MIN_DIST_BETWEEN_AIMS: 3
+}
+
+export function calculatePointCircumference_X(canvasW, angolo) {
+    return (canvasW / 2) + costanti.raggio * Math.cos(angolo);
+
+}
+
+export function calculatePointCircumference_Y(canvasH, angolo) {
+    return canvasH / 2 + costanti.raggio * Math.sin(angolo);
+}
+
+
+export function findNotUsedRandomAngle(arrAngles) {
+
+    let safeAngleFound = false;
+    let angleFound;
+    let attempts = 0
+
+    while (!safeAngleFound && attempts < 100) {
+        attempts++
+
+        angleFound = Math.random() * (2 * Math.PI);
+
+        if (arrAngles.length === 0) {
+            break;
+        }
+
+        arrAngles.some(existingAngle => {
+            if (Math.abs(angleFound - existingAngle) < costanti.MIN_DIST_BETWEEN_AIMS) {
+                console.log("distanza troppo piccola. tenta di nuovo.")
+            } else {
+                safeAngleFound = true
+            }
+        })
+
+    }
+
+    return angleFound
 }
