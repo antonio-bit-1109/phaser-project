84c08e0aac0a16512ed107423ac82f4750bbfede
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-08-23 11:05:48 +0200

    REMOVE: delete updateGameTimer and refreshTime functions from costanti file

--- src/scenes/circleofdeath/constants/costanti.js
+++ src/scenes/circleofdeath/constants/costanti.js
@@ -44,31 +44,3 @@ export function findNotUsedRandomAngle(arrAngles) {
 
     return angleFound
 }
-
-export function updateGameTimer(b, c, d, gameTimer, timerRef) {
-    if (d === 9) {
-        c++;
-        d = 0
-        refreshTime(b, c, d, gameTimer, timerRef)
-        return
-    }
-
-    if (c === 5) {
-        b++
-        c = 0
-        d = 0;
-        refreshTime(b, c, d, gameTimer, timerRef)
-        return
-    }
-
-    d++
-    refreshTime(b, c, d, gameTimer, timerRef)
-    console.log(gameTimer)
-
-}
-
-function refreshTime(b, c, d, gameTimer, timerRef) {
-    gameTimer = `${b}:${c}${d}`;
-    timerRef.setText(gameTimer)
-
-}
