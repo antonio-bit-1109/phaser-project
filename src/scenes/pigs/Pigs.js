11eac18c8e7bfd006dd1d34de5bdacd17a5843db
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-08-27 13:43:57 +0200

    add: initialize canvas dimensions and game name in Pigs scene

--- src/scenes/pigs/Pigs.js
+++ src/scenes/pigs/Pigs.js
@@ -1,12 +1,27 @@
 export class Pigs extends Phaser.Scene {
 
+    canvasWidth = null;
+    canvasHeight = null;
+    gameName = null;
+
+    constructor() {
+        super("pigs");
+    }
+
     init(data) {
+        this.canvasWidth = data.canvasWidth
+        this.canvasHeight = data.canvasHeight
+        this.gameName = data.gameName
     }
 
     preload() {
     }
 
     create() {
+        console.log(this.canvasWidth)
+        console.log(this.canvasHeight)
+        console.log(this.gameName)
+
     }
 
     update() {
