1afa4f53f6a080eee73479b235a767a7e89b5ab8
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-08-27 18:23:10 +0200

    add: implement dude and boss container graphics with volume indicators in Pigs scene

--- src/scenes/pigs/Pigs.js
+++ src/scenes/pigs/Pigs.js
@@ -4,6 +4,14 @@ export class Pigs extends Phaser.Scene {
     canvasHeight = null;
     gameName = null;
 
+    dudeContainer = null
+    dudeContainerVolume = null
+    dudeVolumeValue = 0
+
+    bossContainer = null
+    bossContainerVolume = null
+    bossVolumeValue = 0
+
     constructor() {
         super("pigs");
     }
@@ -35,6 +43,30 @@ export class Pigs extends Phaser.Scene {
             .setDepth(0)
             .setScale(0.5)
 
+        // middle line separation canvas dx e sn
+        const middleLine = this.add.graphics()
+            .fillStyle(0xFFFFFF)
+            .fillRect(this.canvasWidth / 2, 0 / 2, 2, this.canvasHeight)
+        middleLine.strokePath()
+
+        // container lato dude, background
+        this.dudeContainer = this.add.graphics()
+        this.dudeContainer
+            .fillStyle(0xd2d2d2)
+            .fillRect(this.canvasWidth / 20, this.canvasHeight / 18, 35, 300)
+
+        // volume del container che si riempie
+        this.dudeContainerVolume = this.add.graphics()
+        this.dudeContainerVolume
+            .fillStyle(0x008000)
+            .fillRect(this.canvasWidth / 20, this.canvasHeight / 18, 35, 60)
+        //.setRotation(Phaser.Math.DegToRad(180))
+
+        // container lato boss
+        this.bossContainer = this.add.graphics()
+        this.bossContainer
+            .fillStyle(0xd2d2d2)
+            .fillRect(this.canvasWidth / 1.1, this.canvasHeight / 18, 35, 300)
 
     }
 
