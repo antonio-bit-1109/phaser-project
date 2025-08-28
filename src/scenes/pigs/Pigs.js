1ca1f5e0a6b0721d870efaf288d28334dc75a4c3
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-08-28 13:24:17 +0200

    add: integrate AmbientManager for ambient graphics and volume management in Pigs scene

--- src/scenes/pigs/Pigs.js
+++ src/scenes/pigs/Pigs.js
@@ -1,19 +1,22 @@
+import {AmbientManager} from "./managers/AmbientManager";
+
 export class Pigs extends Phaser.Scene {
 
     canvasWidth = null;
     canvasHeight = null;
     gameName = null;
 
-    dudeContainer = null
-    dudeContainerVolume = null
-    dudeVolumeValue = 0
-
-    bossContainer = null
-    bossContainerVolume = null
-    bossVolumeValue = 0
+    // dudeContainer = null
+    // dudeContainerVolume = null
+    // dudeVolumeValue = 0
+    //
+    // bossContainer = null
+    // bossContainerVolume = null
+    // bossVolumeValue = 0
 
     constructor() {
         super("pigs");
+        this.ambientManager = new AmbientManager(this)
     }
 
     init(data) {
@@ -31,43 +34,8 @@ export class Pigs extends Phaser.Scene {
 
     create() {
 
-        this.add.image(this.canvasWidth / 2, this.canvasHeight / 2, "table_poker_bg")
-            .setDepth(-1)
-            .setScale(1.0)
-
-        this.add.image(this.canvasWidth / 9.7, this.canvasHeight / 1.2, "dudePoker")
-            .setDepth(0)
-            .setScale(0.4)
-
-        this.add.image(this.canvasWidth / 1.15, this.canvasHeight / 1.2, "bossPoker")
-            .setDepth(0)
-            .setScale(0.5)
-
-        // middle line separation canvas dx e sn
-        const middleLine = this.add.graphics()
-            .fillStyle(0xFFFFFF)
-            .fillRect(this.canvasWidth / 2, 0 / 2, 2, this.canvasHeight)
-        middleLine.strokePath()
-
-        // container lato dude, background
-        this.dudeContainer = this.add.graphics()
-        this.dudeContainer
-            .fillStyle(0xd2d2d2)
-            .fillRect(this.canvasWidth / 20, this.canvasHeight / 18, 35, 300)
-
-        // volume del container che si riempie
-        this.dudeContainerVolume = this.add.graphics()
-        this.dudeContainerVolume
-            .fillStyle(0x008000)
-            .fillRect(this.canvasWidth / 20, this.canvasHeight / 18, 35, 60)
-        //.setRotation(Phaser.Math.DegToRad(180))
-
-        // container lato boss
-        this.bossContainer = this.add.graphics()
-        this.bossContainer
-            .fillStyle(0xd2d2d2)
-            .fillRect(this.canvasWidth / 1.1, this.canvasHeight / 18, 35, 300)
-
+        this.ambientManager.create(this.canvasWidth, this.canvasHeight)
+        
     }
 
     update() {
