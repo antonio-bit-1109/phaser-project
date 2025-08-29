1c21e45ec0017ecfd4118af428b22485fc04fa33
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-08-29 13:29:40 +0200

    add: implement looping functionality for background songs in SoundsManager

--- src/scenes/circleofdeath/managers/SoundsManager.js
+++ src/scenes/circleofdeath/managers/SoundsManager.js
@@ -1,10 +1,17 @@
 export class SoundsManager {
     scene = null
+    currSongPlayed = null
+    index = 0
 
     constructor(phaserScene) {
         this.scene = phaserScene
         this.mapSounds = new Map()
     }
+
+    getSound(key) {
+        return this.mapSounds.get(key)
+    }
+
 
     resetDefault() {
         this.mapSounds = new Map()
@@ -27,9 +34,47 @@ export class SoundsManager {
         return this.mapSounds.get(key).isPlaying
     }
 
+
     stopAllSounds() {
         for (let sound of this.mapSounds.values()) {
             sound.stop()
         }
+    }
+
+    isAnySoundAlreadyPlaying() {
+        for (let sound of this.mapSounds.values()) {
+
+            if (sound.isPlaying) return true
+
+        }
+        return false
+    }
+
+    //  looping the songs to be able to reproduce it in a loop constantly
+    loopSounds(keys) {
+
+        if (this.isAnySoundAlreadyPlaying()) return
+
+
+        this.currSongPlayed = keys[this.index]
+        !this.isSoundAlreadyPlaying(this.currSongPlayed) && this.playSound(this.currSongPlayed)
+
+
+        this.getSound(this.currSongPlayed).once("complete", () => {
+            console.log("canzone terminata evento acchiappato!!")
+
+            if (this.index === keys.length - 1) {
+                this.index = 0
+            } else {
+
+                this.index++
+            }
+
+            this.currSongPlayed = keys[this.index]
+            // recursion - recall the same method but into the complete event to reproduce again the next song
+            this.loopSounds(keys)
+        })
+
+
     }
 }
